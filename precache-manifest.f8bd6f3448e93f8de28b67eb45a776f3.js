19580d43a957382266db3c159bd4205166ed1ae3
Arvin <arvinhjt0718@gmail.com>
2021-05-18 23:21:35 +1000

    Updates

---
+++ precache-manifest.f8bd6f3448e93f8de28b67eb45a776f3.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7c9734c3b6f1dfb7bf40f35fe56a9a",
+    "url": "/dreggReactWeb/index.html"
+  },
+  {
+    "revision": "fbef79399c9ad03b5e66",
+    "url": "/dreggReactWeb/static/css/main.fad5f2f7.chunk.css"
+  },
+  {
+    "revision": "e6c1edf9f8bed984546f",
+    "url": "/dreggReactWeb/static/js/2.bc66b544.chunk.js"
+  },
+  {
+    "revision": "24f3b8174660712e2ea3ee62d07568c6",
+    "url": "/dreggReactWeb/static/js/2.bc66b544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbef79399c9ad03b5e66",
+    "url": "/dreggReactWeb/static/js/main.1df3ce44.chunk.js"
+  },
+  {
+    "revision": "68be8c75e3bf81553fd7c35052453a64",
+    "url": "/dreggReactWeb/static/js/main.1df3ce44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d5a6b544d80b24df1c",
+    "url": "/dreggReactWeb/static/js/runtime-main.09f3b196.js"
+  },
+  {
+    "revision": "fb65cc75efa326efc662641bb70cdc2d",
+    "url": "/dreggReactWeb/static/media/Arvin Han.fb65cc75.jpg"
+  },
+  {
+    "revision": "686c6728bb931bcb428d32696229ed40",
+    "url": "/dreggReactWeb/static/media/Chinh Do.686c6728.jpg"
+  },
+  {
+    "revision": "325ff1a84313ea6ea2c61c36d1a5efd6",
+    "url": "/dreggReactWeb/static/media/Crimson Mole.325ff1a8.jpg"
+  },
+  {
+    "revision": "f6630e17c3e75052329bafa0d74f2a35",
+    "url": "/dreggReactWeb/static/media/Dr Egg Logo.f6630e17.png"
+  },
+  {
+    "revision": "52434eab524813bf23681c5876f9fb71",
+    "url": "/dreggReactWeb/static/media/IMG_1927.52434eab.jpg"
+  },
+  {
+    "revision": "d4b64aaa383db6fadb6063e8e9a5a090",
+    "url": "/dreggReactWeb/static/media/Jennyfer Ong.d4b64aaa.png"
+  },
+  {
+    "revision": "60dcd0411fdbb73c73deaa7a8f8862b7",
+    "url": "/dreggReactWeb/static/media/Lia Zalums.60dcd041.jpg"
+  },
+  {
+    "revision": "a0c20a8125b1c0d90cf7a7e3153c7773",
+    "url": "/dreggReactWeb/static/media/Lindy walsh.a0c20a81.jpg"
+  },
+  {
+    "revision": "fbd9dba9b547b56353a779dcb73aea46",
+    "url": "/dreggReactWeb/static/media/Minjie Shi.fbd9dba9.JPG"
+  },
+  {
+    "revision": "432a1ce0baf699dc38d58b5c6fc25ff3",
+    "url": "/dreggReactWeb/static/media/Monica Sanki.432a1ce0.jpg"
+  },
+  {
+    "revision": "ab044e70bbd95c5ce4b18019095718c9",
+    "url": "/dreggReactWeb/static/media/Pigeon.ab044e70.png"
+  },
+  {
+    "revision": "9baba68dc341498a3c3a5a3337b1851f",
+    "url": "/dreggReactWeb/static/media/Saba Saeidi.9baba68d.jpg"
+  },
+  {
+    "revision": "87379ff4feea7a8c81ac0169558da002",
+    "url": "/dreggReactWeb/static/media/Saima Ali.87379ff4.JPG"
+  },
+  {
+    "revision": "1bcc43bd65ff18d4bd29eb2885e12fb1",
+    "url": "/dreggReactWeb/static/media/Spy.1bcc43bd.png"
+  },
+  {
+    "revision": "acc2ac45fda5b696244934a0e1c78f0b",
+    "url": "/dreggReactWeb/static/media/WorldConcept.acc2ac45.png"
+  },
+  {
+    "revision": "2ae3f9ecc812ca146d952de9c8e8c0ba",
+    "url": "/dreggReactWeb/static/media/denys.2ae3f9ec.jpg"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/dreggReactWeb/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "182796a27d309b897e77f616df128522",
+    "url": "/dreggReactWeb/static/media/fabien-bazanegue.182796a2.jpg"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/dreggReactWeb/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/dreggReactWeb/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "ddaf636676f8b04cdec8cdb40750277f",
+    "url": "/dreggReactWeb/static/media/james.ddaf6366.jpg"
+  },
+  {
+    "revision": "c085e83e15808eff561f13f30ce937c2",
+    "url": "/dreggReactWeb/static/media/landing-page.c085e83e.png"
+  },
+  {
+    "revision": "96764d80118b4ece734dc14401283b76",
+    "url": "/dreggReactWeb/static/media/lora.96764d80.jpg"
+  },
+  {
+    "revision": "3a749d183532590fda1c14345b6ccca6",
+    "url": "/dreggReactWeb/static/media/mark-finn.3a749d18.jpg"
+  },
+  {
+    "revision": "be4a1748d0f94c35e0a661164495d160",
+    "url": "/dreggReactWeb/static/media/mike.be4a1748.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/dreggReactWeb/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/dreggReactWeb/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/dreggReactWeb/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/dreggReactWeb/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/dreggReactWeb/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/dreggReactWeb/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/dreggReactWeb/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/dreggReactWeb/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "2515c8e5bccbb8334071a0cce1bb2540",
+    "url": "/dreggReactWeb/static/media/profile-page.2515c8e5.png"
+  },
+  {
+    "revision": "9d4a558e7169ed763a05715627bc088c",
+    "url": "/dreggReactWeb/static/media/ryan.9d4a558e.jpg"
+  },
+  {
+    "revision": "79c1998fb18c2d0078bee11f902364e3",
+    "url": "/dreggReactWeb/static/media/square-purple-1.79c1998f.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/dreggReactWeb/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/dreggReactWeb/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/dreggReactWeb/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/dreggReactWeb/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/dreggReactWeb/static/media/square5.ff004a0e.png"
+  }
+]);
